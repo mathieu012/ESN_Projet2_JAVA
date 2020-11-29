ba663b7a13b2d24fa871aaa9c2513761fd0dab87
mathieu012 <almon.mathieu@orange.fr>
2020-11-29 11:46:42 +0100

    entite

---
+++ Inventaire_Mathieu_Nicolas/src/Entite/Inventaire.java
@@ -0,0 +1,95 @@
+package Entite;
+
+public class Inventaire {
+	
+	private int numero_inventaire;
+	private String date;
+	private String article;
+	private int numeroLot;
+	private int numeroSerie;
+	private String lieuStockage;
+	private String emplacement;
+	private int quantite;
+	
+	public Inventaire(int numero_inventaire, String date,String article,int numeroLot,int numeroSerie,String lieuStockage,String emplacement,int quantite) {
+		
+		super();
+		this.numero_inventaire = numero_inventaire;
+		this.date = date;
+		this.article = article;
+		this.numeroLot = numeroLot;
+		this.numeroSerie = numeroSerie;
+		this.lieuStockage = lieuStockage;
+		this.emplacement = emplacement;
+		this.quantite = quantite;
+		
+	}
+
+	public int getNumero_inventaire() {
+		return numero_inventaire;
+	}
+
+	public void setNumero_inventaire(int numero_inventaire) {
+		this.numero_inventaire = numero_inventaire;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getArticle() {
+		return article;
+	}
+
+	public void setArticle(String article) {
+		this.article = article;
+	}
+
+	public int getNumeroLot() {
+		return numeroLot;
+	}
+
+	public void setNumeroLot(int numeroLot) {
+		this.numeroLot = numeroLot;
+	}
+
+	public int getNumeroSerie() {
+		return numeroSerie;
+	}
+
+	public void setNumeroSerie(int numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+
+	public String getLieuStockage() {
+		return lieuStockage;
+	}
+
+	public void setLieuStockage(String lieuStockage) {
+		this.lieuStockage = lieuStockage;
+	}
+
+	public String getEmplacement() {
+		return emplacement;
+	}
+
+	public void setEmplacement(String emplacement) {
+		this.emplacement = emplacement;
+	}
+
+	public int getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	
+	
+	
+
+}
