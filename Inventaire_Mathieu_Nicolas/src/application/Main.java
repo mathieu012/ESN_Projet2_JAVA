3097c40258b4ffe0af7688538237ab465b0fab42
mathieu012 <almon.mathieu@orange.fr>
2020-11-29 10:19:16 +0100

    Interface

---
+++ Inventaire_Mathieu_Nicolas/src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+
+			Parent rootFXML = FXMLLoader.load(getClass().getResource("vue.fxml"));
+
+			Scene sceneFXML = new Scene(rootFXML);
+
+			primaryStage.setTitle("Inventaire");
+
+			primaryStage.setScene(sceneFXML);
+
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
