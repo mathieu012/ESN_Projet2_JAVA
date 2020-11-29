51f040a82518f70ca80243853a3311d70cfbf0d1
mathieu012 <almon.mathieu@orange.fr>
2020-11-29 19:48:38 +0100

    Mise en place de la gestion d'erreur

--- Inventaire_Mathieu_Nicolas/src/application/Controller.java
+++ Inventaire_Mathieu_Nicolas/src/application/Controller.java
@@ -6,6 +6,7 @@ import Model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -30,33 +31,44 @@ public class Controller {
 
 	@FXML
 	private TextField txtQuantite;
+	
+	@FXML
+	private Label txtRetour;
 
 	@FXML
 	private Button btnValider;
 
 	@FXML
 	private Button btnAnnuler;
+	
+	
 
 	Model model = new Model();
 
 	public void valider(ActionEvent e) {
-		
+
 		try {
+
 			//Date
 			Date date = new Date();
+			
 			//Conversion
 			model.conversion(txtIventaire.getText(), date, txtArticle.getText(), txtNumero_lot.getText(), txtNumero_serie.getText(), txtLieu_stockage.getText(), txtEmplacement.getText(), txtQuantite.getText());
+			
 			System.out.println(model.getInventaire().getDate());
+
+		}catch(IOException e1) {
 			
-		} catch (IOException e1) {
+			txtRetour.setText("Cara");
 			
-			
-			e1.printStackTrace();
-			
+		}catch (NumberFormatException e2) {
+
+			txtRetour.setText("Vous n'avez renseigner aucun champ !");
+
 		}
-		
+
 		//Etape 2 appel de l'objet
-		
+
 		//Etape 3 Stockage
 	}
 
